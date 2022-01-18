0804dc97ac1bf4bbbc6ccf24520ee46adb4b550c
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-18 11:53:00 -0800

    Complete functionality for 'Like' button

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,11 @@
 // Adds timer functionality
 const timer = document.getElementById('counter')
 let time = 0
+const timeObj = {}
 function updateTimer() {
     timer.innerText = time + 1
     time = time + 1
+    timeObj[`${time}`] = 0
 }
 setInterval(updateTimer, 1000)
 
@@ -28,16 +30,24 @@ function removeTime() {
 const heartButton = document.getElementById('heart')
 const listOfLikes = document.getElementById('likes')
 const li = document.createElement('li')
-let likes = 0
+
 heartButton.addEventListener('click', loveMe)
 // This function has to log the number
 // and the number of times liked
+let likes = 0
 function loveMe() {
+    let likeText
+    timeObj[time] = timeObj[time] + 1
+    likes = timeObj[time]
     if (likes === 1) {
-        let likeText = document.createTextNode(`${time} has been liked ${likes} time`)
+        likeText = document.innerText = `${time} has been liked ${likes} time`
     } else {
-        let likeText = document.createTextNode(`${time} has been liked ${likes} times`)
+        likeText = document.innerText = `${time} has been liked ${likes} times`
     }
-    li.appendChild(likeText)
-    listOfLikes.appendChild(li)
+
+    const myUl = document.getElementById('list')
+    const myLi = document.createElement('li')
+    myLi.innerText = `${likeText}`
+    myUl.append(myLi)
 }
+
